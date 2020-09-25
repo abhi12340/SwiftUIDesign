440b8662edb73fc254f97523c06c02a14ff26b65
Abhishek kumar <kumara07@mscdirect.com>
2020-09-25 15:05:27 +0530

    animation session completed

--- DesignProject/ContentView.swift
+++ DesignProject/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("UI Design")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Certificate")
-                    .foregroundColor(Color("accent"))
-            }
-            Spacer()
-            Image("Logo1")
+        ZStack {
+            
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            BackCardView()
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(10))
+                .rotation3DEffect(
+                    .degrees(10),axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            
+            BackCardView()
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotation3DEffect(
+                    .degrees(5),axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+            
+            CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+                }
+            
+            BottonCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
         }
     }
 }
@@ -26,5 +58,81 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Credit Card")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Points")
+                        .foregroundColor(Color("accent"))
+                }
+                .padding(.top , 20)
+                .padding(.horizontal , 20)
+                Spacer()
+                Image("Logo1")
+            }
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }.frame(width: 340.0, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }.frame(width: 340, height: 220)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Text("Manage Expenses")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottonCardView: View {
+    var body: some View {
+        VStack(spacing : 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("Mange your personal Expense Using Expense Managment Application with friendly user interface.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal ,20)
+        .frame(maxWidth : .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
     }
 }
